345eec96a8d205d0f51f9791685d23d5b5c63b14
inukatechyt <154017084+inukatechyt@users.noreply.github.com>
2023-12-20 09:02:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ function submitForm() {
         document.getElementById('submitButton').classList.remove('loading');
     }, 3000); // Change 3000 to your actual form submission time in milliseconds
 }
+document.addEventListener('contextmenu', function(e){
+    alert("Sorry, right click is disabled by Inuka to prevent leakage of confidential functions");
+    e.preventDefault();
+    });
+
+    document.onkeydown = function(e) {
+        if(event.keyCode == 123) {
+           return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+           return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+           return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+           return false;
+        }
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+           return false;
+        }
+          if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+           return false;
+        }
+      }
